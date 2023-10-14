d745f1d0ad1c412bc31358cab0eb781fc7185d44
amosmurmu <amosmurmu002@gmail.com>
2023-10-14 16:02:09 +0530

    using custom button tailwind

--- js/script.js
+++
@@ -1,7 +0,0 @@
-    $(document).ready(function () {
-        // Initialize the Bootstrap navbar component
-        $('.navbar-toggler').click(function () {
-            $('#navbarNav').toggleClass('show');
-        });
-    });
-
