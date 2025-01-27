19f5c9eaecdb094651b847a034ac6ee9e6f8fd91
amosmurmu <amosmurmu002@gmail.com>
2025-01-27 19:04:02 +0530

    fix:base in vite with dist

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/amosmurmu.github.io",
+  base: "/amosmurmu.github.io/dist/",
 });
